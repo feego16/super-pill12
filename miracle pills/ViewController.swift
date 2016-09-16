ae9026a52cb9e5af71f67a0f5dd42c17252fabbe
feego16 <feego16@gmail.com>
2016-09-16 02:47:29 -0700

    Initial Commit

---
+++ miracle pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miracle pills
+//
+//  Created by orlando grant on 9/16/16.
+//  Copyright © 2016 antonio orlando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
