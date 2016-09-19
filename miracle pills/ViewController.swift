5a506b1719d447c5f76bc044abd527a8feb3c276
feego16 <feego16@gmail.com>
2016-09-18 18:00:56 -0700

    this should be a test  commit

--- miracle pills/ViewController.swift
+++ miracle pills/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, {
 
+    let parishes = ["Westmoreland", "Kingston", "St. MAry","Clarendon","St.Catherine","Trelawny"]
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        
+            {
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+                
 
-
+   
 }
 
